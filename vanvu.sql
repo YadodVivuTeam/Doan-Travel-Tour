ca979438f6524f5b5ffa40cdc5bc0f8fb75b5b1b
Levanvu6702 <133183956+Levanvu6702@users.noreply.github.com>
2023-05-12 22:35:33 +0700

    Add files via upload

---
+++ vanvu.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Users (
+  UserID int primary key identity(1,1),
+  FullName nvarchar(100) not null,
+  Email nvarchar(100) not null,
+  Password nvarchar(50) not null,
+  Phone nvarchar(20) null,
+  Address nvarchar(200) null,
+  UserType nvarchar(50) not null check (UserType in ('Admin', 'User', 'Staff booking', 'Customer care staff', 'Nhân viên marketing', 'Nhân viên kế toán')),
+  IsBlocked bit not null
+);
