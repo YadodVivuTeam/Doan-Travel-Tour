6613d4f0dfbb0737506de02d5e1dffa5063ef064
huy023 <133367363+huy023@users.noreply.github.com>
2023-05-12 22:36:08 +0700

    Add files via upload

---
+++ giahuy.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Booking (
+  BookingID int primary key identity(1,1),
+  TourID int not null,
+  UserID int not null,
+  PickupLocation nvarchar(200) not null,
+  PickupDate datetime not null,
+  ReturnDate datetime not null,
+  HotelName nvarchar(200) null,
+  HotelAddress nvarchar(200) null,
+  PaymentType nvarchar(50) not null check (PaymentType in ('ATM', 'Chuyển khoản')),
+  TotalPrice decimal(18, 2) not null,
+  Status nvarchar(50) not null check (Status in ('Chờ xác nhận', 'Đã xác nhận', 'Đã huỷ', 'Hoàn thành')),
+  foreign key (TourID) references Tour(TourID),
+  foreign key (UserID) references Users(UserID)
+);
